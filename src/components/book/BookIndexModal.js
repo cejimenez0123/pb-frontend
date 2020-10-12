bdf52698208aa336e6de608a20393f90c6c0c4f0
cejimenez0123 <christianjimenez0123@gmail.com>
2020-10-12 12:49:41 -0400

    modal content

--- src/components/book/BookIndexModal.js
+++ src/components/book/BookIndexModal.js
@@ -23,12 +23,9 @@ let [show,setShow]=useState("none")
     <div>
     <h3 onClick={()=>handleShow()}>Books</h3>
         <div  onClick={(e)=>handleModalClose(e)} style={{width: "100%",display: show}} class="modal">
-            <div  class="modal-content">
+            <div style={{width: "600px"}} class="modalContent">
                 <span  class="close">&times;</span>
-
-                    <div draggable  onDrag={blank} className="modalContent">
                         <BookIndex books={props.books}/>
-                    </div>
                 </div>
             </div>
         </div>
